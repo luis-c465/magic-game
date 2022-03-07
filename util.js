f30b51733f6524b4163eec58009dcf2eb92730d0
Ryan <email>
2022-03-07 12:15:04 -0500

    delete function

--- util.js
+++ util.js
@@ -2,6 +2,6 @@
  * @param {string} string
  * @return {string} String with its first letter capitalized
  */
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
+// function capitalizeFirstLetter(string) {
+//   return string.charAt(0).toUpperCase() + string.slice(1);
+// }
