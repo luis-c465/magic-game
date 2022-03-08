1377678f539efe35dc7eaa8af915547573cc36f3
Luis Canada <canadaluis5@gmail.com>
2022-03-08 16:52:17 -0500

    Get levels working

---
+++ Level.js
@@ -0,0 +1,63 @@
+/**
+ * Abstract class for levels
+ *
+ * @typedef {{x: number, y:number, type: "destructible" | "indestructible"}} wallJSON
+ *
+ * @typedef {"walls"} levelJSONObject
+ * @typedef {{walls?: wallJSON}} levelJSON
+ */
+class Level {
+  /**
+   * @param {number} levelNum
+   */
+  constructor(levelNum) {
+    /**
+     * Level JSON
+     * To be used later in the class to load the level
+     *
+     * @type {levelJSON}
+     * @private
+     */
+    this._data = levels[levelNum];
+
+    this._loadData();
+  }
+
+  /**
+   * @private
+   */
+  _loadData() {
+    // FIXME:
+    // Loop through all keys and value pairs in the _data
+    for (let [key, value] of Object.entries(this._data)) {
+      switch (key) {
+        case "walls":
+          wallsLayer.deleteAll();
+          wallsLayer.objects = value.map(this._loadWallJSON);
+          break;
+
+        default:
+          break;
+      }
+    }
+  }
+
+  /**
+   * Converts a JSON object to a wall
+   *
+   * @param {wallJSON} wall
+   * @return {Wall}
+   */
+  _loadWallJSON(wall) {
+    switch (wall.type) {
+      case "indestructible":
+        return new IndestructibleWall(wallsLayer, wall.x, wall.y);
+
+      case "destructible":
+        return new DestructibleWall(wallsLayer, wall.x, wall.y);
+
+      default:
+        return new IndestructibleWall(wallsLayer, wall.x, wall.y);
+    }
+  }
+}
--- main.js
+++ main.js
@@ -19,23 +19,24 @@ var enemiesLayer = new Layer();
 /** @type {Object.<string, Image>} */
 var images = {};
 
+/** @type {Object.<number, levelJSON} */
+var levels = {};
+
 /**
  * Function called before setup
  * Should be used to preload sprites that are used multiple times like Bullet.js bullet sprite
  */
 function preload() {
-  /**
-   * TODO: Preload bullet sprite
-   * Currently ever time a new bullet is created is requests the bullet sprite image from the server
-   * which may cause performance problems when playing on a mobile device or on poor internet connection
-   */
-
+  // Load images
   images.tanks = loadImage("assets/tanks.png");
   images.bullet = loadImage("assets/bullet.png");
   images.iceWand = loadImage("assets/ice-wand.png", (img) => {
     img.resize(50, 50);
   });
   images.iceBullet = loadImage("assets/ice-bullet.png");
+
+  // Load levels
+  levels[1] = loadJSON("assets/level1.json");
 }
 
 function setup() {
@@ -43,10 +44,12 @@ function setup() {
 
   new Player(playerLayer, 250, 250);
 
-  new IndestructibleWall(wallsLayer, 500, 500);
-  new DestructibleWall(wallsLayer, 500, 400);
+  // new IndestructibleWall(wallsLayer, 500, 500);
+  // new DestructibleWall(wallsLayer, 500, 400);
 
   new TestEnemy(enemiesLayer, 400, 400);
+
+  new Level(1);
 
   // FIXME: Will not update canvas size when user resizes browser window!
   createCanvas(windowWidth, windowHeight);
@@ -80,8 +83,6 @@ function generalUpdate() {
 
   playerLayer.update();
   bulletsLayer.update();
-  console.log("Bullets", bulletsLayer.objects.length);
-  console.log("Bullets", bulletsLayer.objects);
   wallsLayer.update();
   wandsLayer.update();
   enemiesLayer.update();
